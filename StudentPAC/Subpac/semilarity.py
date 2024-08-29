525e64aa3761d8915f284c85b8888ef71558e472
tharun9494 <ontimittatharun2002@gmail.com>
2024-08-29 14:22:02 +0530

    in

--- StudentPAC/Subpac/semilarity.py
+++ StudentPAC/Subpac/semilarity.py
@@ -1,1 +1,24 @@
-print("tharun")
+from PAMI.frequentPattern.basic import FPGrowth as alg
+import pandas as pd
+
+# Specify the input parameters
+inputFile = 'Transactional_T10I4D100K (2).csv'
+separator = '\t'
+minimumSupportCountList = [100, 150, 200, 250, 300]
+
+# Initializing an empty DataFrame to store the results
+result = pd.DataFrame(columns=['algorithm', 'minSup', 'patterns', 'runtime', 'memory'])
+
+# Iterating over each minimum support count
+for minSupCount in minimumSupportCountList:
+    # Initializing the FPGrowth algorithm
+    obj = alg.FPGrowth(inputFile, minSup=minSupCount, sep=separator)
+    # Starting the mining process
+    obj.mine()
+    # Storing the results in the DataFrame
+    result = pd.concat([result, pd.DataFrame({'algorithm': 'FPGrowth', 'minSup': minSupCount,
+                                              'patterns': len(obj.getPatterns()), 'runtime': obj.getRuntime(),
+                                              'memory': obj.getMemoryRSS()}, index=[0])], ignore_index=True)
+
+# Printing the result
+print(result)
---
+++ StudentPAC/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tha',
+    version='2024.8.5',
+    author='ontimitta tharun',
+    author_email='ontimittatharun2002@gmail.com',
+    description='This software is being developed at the University of Aizu, Aizu-Wakamatsu, Fukushima, Japan',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    url='https://github.com/tharun9494/MITS_PAMI',
+    license='GPLv3',
+    install_requires=[            # All necessary packages utilized by our PAMI software
+        'psutil',
+        'pandas',
+        'plotly',
+        'matplotlib',
+        'resource',
+        'validators',
+        'urllib3',
+        'Pillow',
+        'numpy',
+        'sphinx',
+        'sphinx-rtds-theme',
+        'validators',
+        'discord.py',
+        'networkx',
+        'deprecated',
+    ],
+    extras_require={
+        'gpu':  ['cupy', 'pycuda'],
+        'spark': ['pyspark'],
+        'dev': ['twine', 'setuptools', 'build'],
+        'all': ['cupy', 'pycuda', 'pyspark', 'twine', 'setuptools', 'build']
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+)
